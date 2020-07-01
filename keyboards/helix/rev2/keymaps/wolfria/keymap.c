ac363b529348690acfb3e2768c93315ad79af9b4
kiryu-h <kiryu-h@illmatics.co.jp>
2020-07-01 22:28:36 +0900

    My temporary helix key config (The documents are untouched.)

---
+++ keyboards/helix/rev2/keymaps/wolfria/keymap.c
@@ -0,0 +1,418 @@
+#include QMK_KEYBOARD_H
+#include "bootloader.h"
+#ifdef PROTOCOL_LUFA
+#include "lufa.h"
+#include "split_util.h"
+#endif
+#ifdef SSD1306OLED
+  #include "ssd1306.h"
+#endif
+
+
+#ifdef RGBLIGHT_ENABLE
+//Following line allows macro to read current RGB settings
+extern rgblight_config_t rgblight_config;
+#endif
+
+extern uint8_t is_master;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum layer_number {
+    _QWERTY = 0,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+  BACKLIT,
+  EISU,
+  KANA,
+  RGBRST,
+  CUT,
+  COPY,
+  PASTE
+};
+
+enum macro_keycodes {
+  KC_SAMPLEMACRO,
+};
+
+//Macros
+#define M_SAMPLE M(KC_SAMPLEMACRO)
+
+#if MATRIX_ROWS == 8 // HELIX_ROWS == 4
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /*
+   * Qwerty
+   */
+  [_QWERTY] = LAYOUT( \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
+      CUT,     COPY,    PASTE,   KC_LALT, KC_LGUI, LOWER, KC_SPC,        KC_ENT, RAISE,   KC_RALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  ),
+
+  /*
+   * Lower
+   */
+  [_LOWER] = LAYOUT( \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+      KC_LCTL, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, KC_PEQL,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+      KC_LSFT, _______, _______, _______, _______, _______,                        KC_UNDO, KC_AGIN, _______, _______, _______, _______, \
+      CUT,     COPY,    PASTE,   KC_LALT, KC_LGUI, _______, KC_SPC,        KC_ENT, ADJUST,  KC_RALT, _______, _______, _______, _______  \
+  ),
+
+  /*
+   * Raise
+   */
+  [_RAISE] = LAYOUT( \
+      KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+      KC_LCTL, KC_GRV,  KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,                         KC_LT,   KC_MINS, KC_GT,   KC_LBRC, KC_RBRC, KC_PIPE, \
+      KC_LSFT, KC_TILD, _______, _______, _______, _______,                        _______, _______, _______, KC_LCBR, KC_RCBR, KC_BSLS, \
+      CUT,     COPY,    PASTE,   KC_LALT, KC_LGUI, ADJUST,  KC_SPC,        KC_ENT, _______, KC_RALT, _______, _______, _______, _______  \
+  ),
+
+  /* 
+   *　Adjust (Lower + Raise)
+   */
+  [_ADJUST] =  LAYOUT( \
+      RGB_TOG,  _______, _______, _______, _______,  _______,                          KC_MPLY, KC_MUTE, _______, _______, _______, RESET,   \
+      RGB_MOD,  RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,  _______,                          KC_MFFD, KC_VOLU, _______, _______, _______, AG_NORM, \
+      RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,  _______,                          KC_MRWD, KC_VOLD, _______, _______, _______, AG_SWAP, \
+      _______,  _______, _______, _______, _______,  KC_TRNS, _______,        _______, KC_TRNS, _______, _______, _______, _______, _______  \
+  )
+};
+
+#else
+#error "undefined keymaps"
+#endif
+
+// define variables for reactive RGB
+bool TOG_STATUS = false;
+int RGB_current_mode;
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+// Setting ADJUST layer RGB back to default
+void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
+    #ifdef RGBLIGHT_ENABLE
+      //rgblight_mode(RGB_current_mode);
+    #endif
+    layer_on(layer3);
+  } else {
+    layer_off(layer3);
+  }
+}
+
+static bool lgui_pressed = false;
+static uint16_t lgui_pressed_time = 0;
+static bool ralt_pressed = false;
+static uint16_t ralt_pressed_time = 0;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+          //not sure how to have keyboard check mode and set it to a variable, so my work around
+          //uses another variable that would be set to true after the first time a reactive key is pressed.
+        if (TOG_STATUS) { //TOG_STATUS checks is another reactive key currently pressed, only changes RGB mode if returns false
+        } else {
+          TOG_STATUS = !TOG_STATUS;
+          #ifdef RGBLIGHT_ENABLE
+            //rgblight_mode(RGBLIGHT_MODE_SNAKE + 1);
+          #endif
+        }
+        layer_on(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      } else {
+        #ifdef RGBLIGHT_ENABLE
+          //rgblight_mode(RGB_current_mode);   // revert RGB to initial mode prior to RGB mode change
+        #endif
+        TOG_STATUS = false;
+        layer_off(_LOWER);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        //not sure how to have keyboard check mode and set it to a variable, so my work around
+        //uses another variable that would be set to true after the first time a reactive key is pressed.
+        if (TOG_STATUS) { //TOG_STATUS checks is another reactive key currently pressed, only changes RGB mode if returns false
+        } else {
+          TOG_STATUS = !TOG_STATUS;
+          #ifdef RGBLIGHT_ENABLE
+            //rgblight_mode(RGBLIGHT_MODE_SNAKE);
+          #endif
+        }
+        layer_on(_RAISE);
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      } else {
+        #ifdef RGBLIGHT_ENABLE
+          //rgblight_mode(RGB_current_mode);  // revert RGB to initial mode prior to RGB mode change
+        #endif
+        layer_off(_RAISE);
+        TOG_STATUS = false;
+        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+        if (record->event.pressed) {
+          layer_on(_ADJUST);
+        } else {
+          layer_off(_ADJUST);
+        }
+        return false;
+        break;
+      //led operations - RGB mode change now updates the RGB_current_mode to allow the right RGB mode to be set after reactive keys are released
+    case RGB_MOD:
+      #ifdef RGBLIGHT_ENABLE
+        if (record->event.pressed) {
+          rgblight_mode(RGB_current_mode);
+          rgblight_step();
+          RGB_current_mode = rgblight_config.mode;
+        }
+      #endif
+      return false;
+      break;
+    case EISU:
+      if (record->event.pressed) {
+        if(keymap_config.swap_lalt_lgui==false){
+          register_code(KC_LANG2);
+        }else{
+          SEND_STRING(SS_LALT("`"));
+        }
+      } else {
+        unregister_code(KC_LANG2);
+      }
+      return false;
+      break;
+    case KANA:
+      if (record->event.pressed) {
+        if(keymap_config.swap_lalt_lgui==false){
+          register_code(KC_LANG1);
+        }else{
+          SEND_STRING(SS_LALT("`"));
+        }
+      } else {
+        unregister_code(KC_LANG1);
+      }
+      return false;
+      break;
+    case RGBRST:
+      #ifdef RGBLIGHT_ENABLE
+        if (record->event.pressed) {
+          eeconfig_update_rgblight_default();
+          rgblight_enable();
+          RGB_current_mode = rgblight_config.mode;
+        }
+      #endif
+      break;
+    case CUT:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LGUI("x"));
+      }
+      break;
+    case COPY:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LGUI("c"));
+      }
+      break;
+    case PASTE:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LGUI("v"));
+      }
+      break;
+    case KC_LGUI:
+      if (record->event.pressed) {
+        register_code(KC_LGUI);
+        
+        lgui_pressed = true;
+        lgui_pressed_time = record->event.time;
+      } else {
+        unregister_code(KC_LGUI);
+
+        if (lgui_pressed && (TIMER_DIFF_16(record->event.time, lgui_pressed_time) < TAPPING_TERM)) {
+          register_code(KC_LANG2);
+          unregister_code(KC_LANG2);
+        }
+        lgui_pressed = false;
+      }
+      return false;
+      break;
+    case KC_RALT:
+      if (record->event.pressed) {
+        register_code(KC_RALT);
+
+        ralt_pressed = true;
+        ralt_pressed_time = record->event.time;
+      } else {
+        unregister_code(KC_RALT);
+
+        if (ralt_pressed && (TIMER_DIFF_16(record->event.time, ralt_pressed_time) < TAPPING_TERM)) {
+          register_code(KC_LANG1);
+          unregister_code(KC_LANG1);
+        }
+        ralt_pressed = false;
+      }
+      return false;
+      break;
+    default:
+      if (record->event.pressed) {
+        // reset the flag
+        lgui_pressed = false;
+        ralt_pressed = false;
+      }
+      break;
+  }
+  return true;
+}
+
+void matrix_init_user(void) {
+  #ifdef RGBLIGHT_ENABLE
+    RGB_current_mode = rgblight_config.mode;
+  #endif
+  //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
+  #ifdef SSD1306OLED
+      iota_gfx_init(!has_usb());   // turns on the display
+  #endif
+}
+
+//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
+#ifdef SSD1306OLED
+
+void matrix_scan_user(void) {
+  iota_gfx_task();  // this is what updates the display continuously
+}
+
+void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
+  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
+    memcpy(dest->display, source->display, sizeof(dest->display));
+    dest->dirty = true;
+  }
+}
+
+//assign the right code to your layers for OLED display
+#define L_BASE 0
+#define L_LOWER (1<<_LOWER)
+#define L_RAISE (1<<_RAISE)
+#define L_ADJUST (1<<_ADJUST)
+#define L_ADJUST_TRI (L_ADJUST|L_RAISE|L_LOWER)
+
+static void render_logo(struct CharacterMatrix *matrix) {
+  static const char helix_logo[] PROGMEM ={
+    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,
+    0};
+  matrix_write_P(matrix, helix_logo);
+  //matrix_write_P(&matrix, PSTR(" Split keyboard kit"));
+}
+
+static void render_rgbled_status(bool full, struct CharacterMatrix *matrix) {
+  #ifdef RGBLIGHT_ENABLE
+    char buf[30];
+    if (RGBLIGHT_MODES > 1 && rgblight_config.enable) {
+      if (full) {
+          snprintf(buf, sizeof(buf), " LED %2d: %d,%d,%d ", rgblight_config.mode, rgblight_config.hue / RGBLIGHT_HUE_STEP, rgblight_config.sat / RGBLIGHT_SAT_STEP, rgblight_config.val / RGBLIGHT_VAL_STEP);
+      } else {
+          snprintf(buf, sizeof(buf), "[%2d] ", rgblight_config.mode);
+      }
+      matrix_write(matrix, buf);
+    }
+  #endif
+}
+
+static void render_layer_status(struct CharacterMatrix *matrix) {
+  // Define layers here, Have not worked out how to have text displayed for each layer. Copy down the number you see and add a case for it below
+  char buf[10];
+  matrix_write_P(matrix, PSTR("Layer: "));
+  switch (layer_state) {
+    case L_BASE:
+      matrix_write_P(matrix, PSTR("Default"));
+      break;
+    case L_RAISE:
+      matrix_write_P(matrix, PSTR("Raise"));
+      break;
+    case L_LOWER:
+      matrix_write_P(matrix, PSTR("Lower"));
+      break;
+    case L_ADJUST:
+    case L_ADJUST_TRI:
+    case 10:
+    case 12:
+      matrix_write_P(matrix, PSTR("Adjust"));
+      break;
+    default:
+      matrix_write_P(matrix, PSTR("Undef-"));
+      snprintf(buf, sizeof(buf), "%ld", layer_state);
+      matrix_write(matrix, buf);
+  }
+}
+
+void render_status(struct CharacterMatrix *matrix) {
+  // Render to mode icon
+  static const char os_logo[][2][3] PROGMEM = {{{0x95, 0x96, 0}, {0xb5, 0xb6, 0}}, {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}}};
+  if (keymap_config.swap_lalt_lgui == false) {
+    matrix_write_P(matrix, os_logo[0][0]);
+    matrix_write_P(matrix, PSTR("\n"));
+    matrix_write_P(matrix, os_logo[0][1]);
+  } else {
+    matrix_write_P(matrix, os_logo[1][0]);
+    matrix_write_P(matrix, PSTR("\n"));
+    matrix_write_P(matrix, os_logo[1][1]);
+  }
+
+  matrix_write_P(matrix, PSTR(" "));
+  render_layer_status(matrix);
+  matrix_write_P(matrix, PSTR("\n"));
+
+  // Host Keyboard LED Status
+  matrix_write_P(matrix, (host_keyboard_leds() & (1 << USB_LED_NUM_LOCK)) ? PSTR("NUMLOCK") : PSTR("       "));
+  matrix_write_P(matrix, (host_keyboard_leds() & (1 << USB_LED_CAPS_LOCK)) ? PSTR("CAPS") : PSTR("    "));
+  matrix_write_P(matrix, (host_keyboard_leds() & (1 << USB_LED_SCROLL_LOCK)) ? PSTR("SCLK") : PSTR("    "));
+  matrix_write_P(matrix, PSTR("\n"));
+  render_rgbled_status(true, matrix);
+}
+
+void iota_gfx_task_user(void) {
+  struct CharacterMatrix matrix;
+
+  #if DEBUG_TO_SCREEN
+    if (debug_enable) {
+      return;
+    }
+  #endif
+
+  matrix_clear(&matrix);
+  if (is_master) {
+    render_status(&matrix);
+  } else {
+    render_logo(&matrix);
+    render_rgbled_status(false, &matrix);
+    render_layer_status(&matrix);
+  }
+  matrix_update(&display, &matrix);
+}
+
+#endif
